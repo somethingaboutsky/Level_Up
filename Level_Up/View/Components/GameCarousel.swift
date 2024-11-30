c9f9565f413e8ad2268bf2518ab738c209a4fe7c
justdoit1897 <vincenzofardella96@gmail.com>
2024-11-30 10:17:11 +0100

    Created homepage view and updated accent color.

---
+++ Level_Up/View/Components/GameCarousel.swift
@@ -0,0 +1,60 @@
+//
+//  ContentCarousel.swift
+//  Level_Up
+//
+//  Created by Vincenzo Fardella on 29/11/24.
+//
+
+import SwiftUI
+
+struct GameCarousel: View {
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    let totalPages: Int = 5
+    
+    @State private var currentPage: Int = 0
+    
+    var body: some View {
+
+        VStack {
+            TabView (selection: $currentPage) {
+                ForEach(0..<totalPages) { i in
+                    RoundedRectangle(cornerRadius: 32)
+                        .fill(Color(red: 43.0/255.0, green: 43.0/255.0, blue: 43.0/255.0))
+                        .frame(width: screenWidth * 0.8, height: screenHeight * 0.5858124)
+                        .overlay(VStack(alignment: .center) {
+                            Image(.acLogo)
+                                .resizable()
+                                .frame(width: screenWidth * 0.8756219, height: screenHeight * 0.40274599542334094)
+                                .shadow(color: Color(red:212.0/255.0, green: 71.0/255.0, blue: 236.0/255.0), radius: 24, x: 0, y: 0)
+                            Text("Assassin's Creed \(i+1)")
+                                .font(.system(size: 28, weight: .semibold))
+                                .padding(.vertical, 10)
+                            Text("Release Year \(i+1)")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .frame(alignment: .center)
+                        .padding(.horizontal, 10)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            
+            // Custom page control
+            HStack(spacing: 8) {
+                ForEach(0..<totalPages, id: \.self) { page in
+                    Circle()
+                        .fill(page == currentPage ? .accentColor : Color.gray)
+                        .frame(width: 10, height: 10)
+                        .animation(.easeInOut, value: currentPage)
+                }
+            }
+            .padding()
+        }
+        .frame(width: screenWidth, height: screenHeight * 0.83)
+        //.padding(.bottom, 20)
+    }
+}
+
+#Preview {
+    GameCarousel()
+}
---
+++ Level_Up/View/Components/MoviesCarousel.swift
@@ -0,0 +1,62 @@
+//
+//  MoviesCarousel.swift
+//  Level_Up
+//
+//  Created by Vincenzo Fardella on 29/11/24.
+//
+
+import SwiftUI
+
+struct MoviesCarousel: View {
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    let totalPages = 3
+    
+    @State private var currentPage: Int = 0
+    
+    var body: some View {
+        VStack {
+            Text("Related Movies")
+                .font(.system(size: 28, weight: .semibold))
+                .padding(.leading, 10)
+                .padding(.bottom, 0)
+            TabView (selection: $currentPage) {
+                ForEach(0..<totalPages) { i in
+                    RoundedRectangle(cornerRadius: 32)
+                        .fill(Color(red: 43.0/255.0, green: 43.0/255.0, blue: 43.0/255.0))
+                        .frame(width: screenWidth * 0.8, height: screenHeight * 0.5858124)
+                        .overlay(VStack(alignment: .center) {
+                            Image(.acLogo)
+                                .resizable()
+                                .frame(width: screenWidth * 0.8756219, height: screenHeight * 0.40274599542334094)
+                                .shadow(color: Color(red:212.0/255.0, green: 71.0/255.0, blue: 236.0/255.0), radius: 24, x: 0, y: 0)
+                            Text("Assassin's Creed Movie \(i+1)")
+                                .font(.system(size: 28, weight: .semibold))
+                                .padding(.vertical, 10)
+                            Text("Release Year \(i+1)")
+                        })
+                        .frame(maxWidth: .infinity)
+                        .frame(alignment: .center)
+                        .padding(.horizontal, 10)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            
+            // Custom page control
+            HStack(spacing: 8) {
+                ForEach(0..<totalPages, id: \.self) { page in
+                    Circle()
+                        .fill(page == currentPage ? .accentColor : Color.gray)
+                        .frame(width: 10, height: 10)
+                        .animation(.easeInOut, value: currentPage)
+                }
+            }
+            .padding()
+        }
+        .frame(width: screenWidth, height: screenHeight * 0.8)
+    }
+}
+
+#Preview {
+    MoviesCarousel()
+}

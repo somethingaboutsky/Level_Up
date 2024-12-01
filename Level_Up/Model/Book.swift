44e59cece1f4fabdb08feebce0900b8909fd1f47
justdoit1897 <vincenzofardella96@gmail.com>
2024-12-02 00:29:54 +0100

    Final Home Update (2024-12-02, 00:29)

---
+++ Level_Up/Model/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Books.swift
+//  Try
+//
+//  Created by Gabriele Apicella on 28/11/24.
+//
+
+import Foundation
+
+struct Book: Codable {
+    var id: Int
+    var title: String
+    var cover: String
+    var releaseYear: Int
+}
---
+++ Level_Up/Model/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Films.swift
+//  Try
+//
+//  Created by Gabriele Apicella on 28/11/24.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    var id: Int
+    var title: String
+    var cover: String
+    var releaseYear: Int
+}

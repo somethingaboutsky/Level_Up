dccf524e39e75f0e2b1b7a66cd24f4e51702eb17
justdoit1897 <vincenzofardella96@gmail.com>
2024-12-01 11:46:54 +0100

    Optimised view for iPhone 14

---
+++ Level_Up/View/Story.swift
@@ -0,0 +1,27 @@
+//
+//  Story.swift
+//  Level_Up
+//
+//  Created by Vincenzo Fardella on 30/11/24.
+//
+
+import SwiftUI
+
+struct Story: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("Storia")
+                .font(.system(size: 28, weight: .bold, design: .default))
+            
+            
+                
+            
+        }
+        .padding(.horizontal, 10)
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    Story()
+}

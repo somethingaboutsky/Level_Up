622a1350bc908c124a49b93a5ebadcd15e2cc3ab
Gabriele Apicella <gabryapix3@gmail.com>
2024-12-03 16:05:22 +0100

    change to Struct and Db

--- Level_Up/Model/Book.swift
+++ Level_Up/Model/Book.swift
@@ -12,4 +12,5 @@ struct Book: Codable {
     var title: String
     var cover: String
     var releaseYear: Int
+    var synopsis: String
 }
--- Level_Up/Model/Movie.swift
+++ Level_Up/Model/Movie.swift
@@ -12,4 +12,5 @@ struct Movie: Codable {
     var title: String
     var cover: String
     var releaseYear: Int
+    var synopsis: String
 }
